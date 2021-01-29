011ebe4515e966c325d6f7a4453273e62bf31236
NishantSushmakar <46222988+NishantSushmakar@users.noreply.github.com>
2021-01-29 17:18:29 +0530

    speech features to be implemented

--- language.py
+++ language.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 27 13:32:32 2021
 
-@author: nishant
-"""
 from googletrans import Translator
 
 
--- meeting.py
+++ meeting.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 19 19:31:14 2021
-
-@author: nishant
-"""
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-from credential import email_id,password,chromedriver_path
+from credential import email_id,password,chromedriver_path,meeting_code
 from text_process import Node, process_data
 from key import get_score
 from bs4 import BeautifulSoup
+from multiprocessing import Process
+
 
 def enter_meeting(browser,email_id,password,meeting_code):
     '''
@@ -70,12 +66,28 @@ def count_meeting(browser):
     
     '''
     
-    count_element = browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[3]/div/div[2]/div[2]/div[1]/div[1]/span/div/span[2]')
+    count_element = browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[4]/div/div[2]/div[2]/div[1]/div[1]/span/div/span[2]')
     
     return int(count_element.text.strip('()')) 
 
 
-
+def process_captions(browser):
+   print("This is Process Captions Function") 
+   while count_meeting(browser)>1:
+       soup = BeautifulSoup(browser.page_source, 'html.parser')
+       mydivs = soup.findAll("div", {"class": "a4cQT xiCV9b"})
+       try:
+           for i in range(len(mydivs)):
+            print(mydivs[i].find('div',{'class':'zs7s8d jxFHg'}).get_text())
+            print('*'*20)
+            for k in range(len(mydivs[i].findAll('span',{'class':'CNusmb'}))):
+                
+                print(mydivs[i].findAll('span',{'class':'CNusmb'})[k].get_text())
+        
+       except:
+           print('AttributeError')
+       time.sleep(8)
+   return browser 
 
 def chat_detect(browser):
     '''
@@ -87,17 +99,13 @@ def chat_detect(browser):
         
     '''
 
-    browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[6]/div[3]/div/div[2]/div[3]/span/span').click()
-    time.sleep(5)
-    chat_element = browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[3]/div/div[2]/div[2]/div[2]/span[2]/div/div[2]')
-    prev_div = 0 
-
+    print("This is Chat Detect Function")
     node = Node()
     offenders_count = {}
 
     while(count_meeting(browser) > 1):
         node, offenders_count = process_data(browser, node, offenders_count)
-        browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[3]/div/div[2]/div[2]/div[1]/div[2]').click()
+        browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[4]/div/div[2]/div[2]/div[1]/div[2]').click()
     
     browser.quit()    
     return 
@@ -111,5 +119,11 @@ if __name__ == "__main__":
     
     browser = webdriver.Chrome(chromedriver_path,chrome_options=chrome_options)
     
-    browser = enter_meeting(browser,email_id,password,'edi-guzq-knb')
-    chat_detect(browser)
+    browser = enter_meeting(browser,email_id,password,meeting_code)
+    browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[1]/div[3]/div/div[2]/div[3]/span/span').click()
+    time.sleep(5)
+    browser.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[8]/div[3]/div[9]/div[3]/div[2]/div/span/span/div').click()
+    time.sleep(5)
+    #chat_detect(browser)
+    process_captions(browser)
+    
